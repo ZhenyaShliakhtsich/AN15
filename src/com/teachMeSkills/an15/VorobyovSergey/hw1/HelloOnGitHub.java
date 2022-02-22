78cda17c59c6cc6c5d5ff25be79eaa59be5208f6
sv <sergei.vorobyov@mail.ru>
2022-02-22 20:34:41 +0300

    Attempt two to connect

---
+++ src/com/teachMeSkills/an15/VorobyovSergey/hw1/HelloOnGitHub.java
@@ -0,0 +1,7 @@
+package com.teachMeSkills.an15.VorobyovSergey.hw1;
+
+public class HelloOnGitHub {
+    public static void main(String[] args) {
+        System.out.println("Hi everyone");
+    }
+}
