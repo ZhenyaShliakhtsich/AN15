71adea437c32ef4d0139d1fe267eb07bad344d96
ZhenyaShliakhtsich <shl.jeka@gmail.com>
2022-02-22 14:03:09 +0300

    Первое задание

--- src/com/teachMeSkills/an15/ShlyakhtichEvgeniy/hw2/StringTasks.java
+++ src/com/teachMeSkills/an15/ShlyakhtichEvgeniy/hw2/StringTasks.java
@@ -4,10 +4,10 @@ public class StringTasks {
     public static void main(String[] args){
         String string = "I like Java!!!";//Напишите параметр, который принимает строку, “I like Java!!!”.
         System.out.println(string.charAt(13));// Распечатать последний символ строки. Используем метод String.charAt().
-        System.out.println(string.endsWith("!!!"))// Проверить, заканчивается ли ваша строка подстрокой “!!!”. Используем метод String.endsWith().
-        System.out.println(string.startsWith("I like"))//Проверить, начинается ли ваша строка подстрокой “I like”. Используем метод String.startsWith().
+        System.out.println(string.endsWith("!!!"));// Проверить, заканчивается ли ваша строка подстрокой “!!!”. Используем метод String.endsWith().
+        System.out.println(string.startsWith("I like"));//Проверить, начинается ли ваша строка подстрокой “I like”. Используем метод String.startsWith().
         System.out.println(string.contains("Java"));//Проверить, содержит ли ваша строка подстроку “Java”. Используем метод String.contains()
-        int a = string.indexOf("Java")//Найти позицию подстроки “Java” в строке “I like Java!!!
+        int a = string.indexOf("Java");//Найти позицию подстроки “Java” в строке “I like Java!!!
         int b = string.lastIndexOf("Java");
         System.out.println(a);//
         System.out.println(string.replace("а","о"));//Заменить все символы “а” на “о”
