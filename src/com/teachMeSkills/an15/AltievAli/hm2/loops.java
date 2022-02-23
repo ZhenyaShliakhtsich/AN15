9531a6e8a7b5ce2941913a7fb7e2943d00f2441f
AliAltiyev <alidroid696@gmail.com>
2022-02-23 13:31:11 +0300

    Не получается

--- src/com/teachMeSkills/an15/AltievAli/hm2/loops.java
+++ src/com/teachMeSkills/an15/AltievAli/hm2/loops.java
@@ -1,5 +1,8 @@
 package com.teachMeSkills.an15.AltievAli.hm2;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class loops {
     public static void main(String[] args) {
 
@@ -7,13 +10,23 @@ public class loops {
         int i = 5;
 
         do {
-            i+=5;
+            i += 5;
             System.out.println(stringBuilder.append(i).append(","));
-        }while (i < 100);
+        } while (i < 100);
 
 
+///Task with coins
+
+        // Random random = new Random(30);
+        // Scanner scanner = new Scanner(System.in);
 
 
+        int i1 = 5;
+        for (i1 = 5; i1 < 100; i1++) {
+            if (i % 5 == 0) System.out.print(i + ", ");
+        }
+        stringBuilder.append(" , ");
+        System.out.println(stringBuilder);
 
     }
 
