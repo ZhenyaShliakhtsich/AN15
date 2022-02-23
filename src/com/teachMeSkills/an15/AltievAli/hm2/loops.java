97867cde0d59d4870c2a043c1384e1aaf38a041f
AliAltiyev <alidroid696@gmail.com>
2022-02-23 13:16:27 +0300

    updated loops

---
+++ src/com/teachMeSkills/an15/AltievAli/hm2/loops.java
@@ -0,0 +1,21 @@
+package com.teachMeSkills.an15.AltievAli.hm2;
+
+public class loops {
+    public static void main(String[] args) {
+
+        StringBuilder stringBuilder = new StringBuilder();
+        int i = 5;
+
+        do {
+            i+=5;
+            System.out.println(stringBuilder.append(i).append(","));
+        }while (i < 100);
+
+
+
+
+
+    }
+
+
+}
