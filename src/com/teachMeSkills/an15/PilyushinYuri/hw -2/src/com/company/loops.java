b0417285f92fbe92237948aa5c9855e6c5e8038f
PilushinYuri <Cheshirskiy96@gmail.com>
2022-02-24 19:27:55 +0300

    ДЗ 1 исправленное

--- src/com/teachMeSkills/an15/PilyushinYuri/hw -2/src/com/company/loops.java
+++ src/com/teachMeSkills/an15/PilyushinYuri/hw -2/src/com/company/loops.java
@@ -10,26 +10,26 @@ public class loops {
         }
         System.out.println(stringBuilder);
 
-         // Задача 3.
-            int num2 = 0;
-            while (num2 < 11) {
-                System.out.println(num2 + "*7 =" + 7 * num2);
-                num2++;
-            }
+        // Задача 3.
+        int num2 = 0;
+        while (num2 < 11) {
+            System.out.println(num2 + "*7 =" + 7 * num2);
+            num2++;
+        }
         // Задача 2.
         int random = 1 + (int) (Math.random() * 31);
-            int num3 = 1;
-            while (num3 < 31){
-                if (num3 == random){
-                    System.out.println("Счастливая монета номер " + random + " найдена");
-                    break;
-                } else {
-                    System.out.println("Надеюсь следующая монета будет счастливой " + num3);
-                }
-                num3 ++;
-
+        int num3 = 1;
+        while (num3 < 31) {
+            if (num3 == random) {
+                System.out.println("Счастливая монета номер " + random + " найдена");
+                break;
+            } else {
+                System.out.println("Надеюсь следующая монета будет счастливой " + num3);
             }
+            num3++;
 
         }
+
     }
+}
 
