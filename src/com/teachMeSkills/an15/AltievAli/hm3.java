e8cf3e439f399fa47a65999f97388fda245f2d5d
AliAltiyev <alidroid696@gmail.com>
2022-02-26 01:08:14 +0300

    !!!

---
+++ src/com/teachMeSkills/an15/AltievAli/hm3.java
@@ -0,0 +1,60 @@
+package com.teachMeSkills.an15.AltievAli;
+
+import java.util.Scanner;
+
+public class hm3 {
+    public static void main(String[] args) {
+        System.out.println("Enter your salary.");
+        Scanner scanner = new Scanner(System.in);
+        int userSalary = scanner.nextInt();
+        System.out.println("Enter month!");
+        int month = scanner.nextInt();
+
+
+
+
+
+
+
+
+
+
+//        String [] months  = new String [12];
+//        months[0] = "January";
+//        months[1] = "February";
+//        months[2] = "March";
+//        months[3] = "April";
+//        months[4] = "May";
+//        months[5] = "June";
+//        months[6] = "July";
+//        months[7] = "August";
+//        months[8] = "September";
+//        months[9] = "October";
+//        months[10] = "November";
+//        months[11] = "December";
+
+//        while (true) {
+//            double userSal = userSalary * 0.7;
+//
+//            System.out.println(userSal );
+//
+//        }
+
+
+
+
+
+
+
+
+
+    }
+
+    public static  double addSevenPercent ( int a){
+
+        return  a * 0.7;
+
+    }
+
+
+}
