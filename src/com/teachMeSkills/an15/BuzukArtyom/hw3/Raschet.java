2d92b730ceaf48935b88bc93c135ae6240ce5abf
ArtyomBuzuk <artyombuzuk16@gmail.com>
2022-02-26 13:18:57 +0300

    1 и 2 задача

---
+++ src/com/teachMeSkills/an15/BuzukArtyom/hw3/Raschet.java
@@ -0,0 +1,30 @@
+package com.teachMeSkills.an15.BuzukArtyom.hw3;
+
+import java.math.BigDecimal;
+
+public class Raschet {
+    double summa;
+    int mes;
+
+    public Raschet(double summa, int mes) {
+        this.summa = summa;
+        this.mes = mes;
+    }
+    public BigDecimal procent() {
+        BigDecimal raschet = new BigDecimal(summa);
+        for (int i = 1; i <= mes; i++) {
+            raschet = raschet.add(raschet.multiply(BigDecimal.valueOf(0.07)));
+        }
+        return raschet;
+    }
+    public double toWhile(){
+        BigDecimal rachet = new BigDecimal(summa);
+        int x = 0;
+        while (x!=mes){
+        summa+=summa*0.07;
+        x++;
+        }
+        return summa;
+    }
+
+}
---
+++ src/com/teachMeSkills/an15/BuzukArtyom/hw3/Vklad.java
@@ -0,0 +1,23 @@
+package com.teachMeSkills.an15.BuzukArtyom.hw3;
+
+import java.util.Scanner;
+
+public class Vklad {
+    public static void main(String[] args){
+//        1. За каждый месяц банк начисляет к сумме вклада 7% от суммы.
+//           Напишите консольную программу, в которую пользователь вводит сумму вклада и количество месяцев.
+//           А банк вычисляет конечную сумму вклада с учетом начисления процентов за каждый месяц.
+//           Вывести в консоль конечную сумму.
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите сумму вашего вклада: ");
+        int summa = scanner.nextInt();
+        System.out.println("Введите количество месяцев: ");
+        int mes = scanner.nextInt();
+        Raschet raschet = new Raschet(summa,mes);
+        System.out.println(raschet.procent());
+        System.out.println("---------------------------------");
+        System.out.println("Через while ");
+        System.out.println(raschet.toWhile());
+
+    }
+}
