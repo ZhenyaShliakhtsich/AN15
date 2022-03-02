219b73cd74bdbfeda0db09246569cde7ecabccd0
AliAltiyev <alidroid696@gmail.com>
2022-03-02 12:10:09 +0300

    hw4/task3

---
+++ src/com/teachMeSkills/an15/AltievAli/hm4/Task3/Main.java
@@ -0,0 +1,29 @@
+package com.teachMeSkills.an15.AltievAli.hm4.Task3;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        int a = 1;
+        int b = 1;
+        int c;
+
+
+        for (int i = 1; i <= 20; i++) {
+
+            c = a + b;
+            a = b;
+            b = c;
+            System.out.print(b + " ");
+
+
+        }
+
+
+    }
+
+
+}
+
+
