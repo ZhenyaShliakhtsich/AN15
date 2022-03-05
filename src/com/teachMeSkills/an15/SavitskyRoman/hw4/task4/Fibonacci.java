990812eac2237616b3ea1c3aa85f632cc0e6de1a
SavitskyRoman <savitskyroman508@gmail.com>
2022-03-05 13:33:34 +0300

    hw4 task 3

---
+++ src/com/teachMeSkills/an15/SavitskyRoman/hw4/task4/Fibonacci.java
@@ -0,0 +1,14 @@
+package com.teachMeSkills.an15.SavitskyRoman.hw4.task4;
+
+import java.util.Arrays;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        int[] x = new int[47];
+        x[1] = 1;
+        for (int i = 2; i < x.length; ++i) {
+            x[i] = x[i - 1] + x[i - 2];
+        }
+        System.out.println(Arrays.toString(x));
+    }
+}
