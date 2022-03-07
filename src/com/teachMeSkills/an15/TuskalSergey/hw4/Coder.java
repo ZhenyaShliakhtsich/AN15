80558a7481f1aa2114dc426d32a48454d6c9ad85
Sergei-AN15 <tuskal92@gmail.com>
2022-03-07 23:36:09 +0300

    Доделал 4 задание из ДЗ-4.

--- src/com/teachMeSkills/an15/TuskalSergey/hw4/Coder.java
+++ src/com/teachMeSkills/an15/TuskalSergey/hw4/Coder.java
@@ -2,39 +2,45 @@ package com.teachMeSkills.an15.TuskalSergey.hw4;
 
 public class Coder {
     String name;
-    String dob;
+    String dateOfBirth;
     String position;
     int salary;
     int phoneNumber;
-    
-    public Coder(String name, String dob, String position, int salary, int phoneNumber) {
+
+    public Coder(String name, String dateOfBirth, String position, int salary, int phoneNumber) {
         this.name = name;
-        this.dob = dob;
+        this.dateOfBirth = dateOfBirth;
         this.position = position;
         this.salary = salary;
         this.phoneNumber = phoneNumber;
     }
 
-public void takeVacation(int daysOfVacation) {
-    System.out.println(name + " взял отпуск на " + daysOfVacation + " день");
-}
+    public void takeVacation(int daysOfVacation) {
+        System.out.println(name + " взял отпуск на " + daysOfVacation + " день");
+    }
 
-public void takeVacation(String country) {
+public void takeVacation(Country country) {
     System.out.println(name + " взял отпуск, чтобы посетить: " + country);
 }
 
-public void returnBack() {
-    System.out.println(name + " вернулся из отпуска");
-}
+    public void takeVacation(PlaceToVisit placeToVisit) {
+        System.out.println(name + " взял отпуск, чтобы посетить: " + placeToVisit);
+    }
+
+    public void returnBack() {
+        System.out.println(name + " вернулся из отпуска");
+    }
 
     @Override
     public String toString() {
         return "Coder{" +
                 "name='" + name + '\'' +
-                ", dob='" + dob + '\'' +
+                ", dateOfBirth='" + dateOfBirth + '\'' +
                 ", position='" + position + '\'' +
                 ", salary=" + salary +
                 ", phoneNumber=" + phoneNumber +
                 '}';
     }
+
+
 }
--- src/com/teachMeSkills/an15/TuskalSergey/hw4/Country.java
+++ src/com/teachMeSkills/an15/TuskalSergey/hw4/Country.java
@@ -1,5 +1,7 @@
 package com.teachMeSkills.an15.TuskalSergey.hw4;
 
+import java.util.Objects;
+
 public class Country {
     String country;
     String capital;
@@ -9,8 +11,25 @@ public class Country {
         this.capital = capital;
     }
 
+    public Country() {
+
+    }
+
     @Override
     public String toString() {
         return country + " - " + capital;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Country country1 = (Country) o;
+        return Objects.equals(country, country1.country) && Objects.equals(capital, country1.capital);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(country, capital);
     }
 }
