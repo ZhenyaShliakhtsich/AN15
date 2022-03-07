fbd3ae20e53018c2269b0932b019b74929acdb7f
Sergei-AN15 <tuskal92@gmail.com>
2022-03-07 14:53:06 +0300

    Обновил задание 1 из ДЗ-4

--- src/com/teachMeSkills/an15/TuskalSergey/hw4/Task1.java
+++ src/com/teachMeSkills/an15/TuskalSergey/hw4/Task1.java
@@ -5,11 +5,18 @@ public class Task1 {
     public static void main(String[] args) {
         Car car1 = new Car("Volkswagen Polo", 100, 10000);
         Car car2 = new Car("Volkswagen Jetta", 150, 15000);
+        Car[] cars = {car1, car2};
 
-        try { car1.start();
-            System.out.println("Машина " + car1.model + " завелась");
-        } catch (Exception e) {
-            System.out.println("Машина " + car1.model + " не завелась");
+        for (int i = 0; i < cars.length; i++) {
+            try {
+                cars[i].start();
+                if (cars[i].start() == false) {
+                    throw new Exception();
+                }
+                System.out.println("Машина " + cars[i].model + " завелась");
+            } catch (Exception f) {
+                System.out.println("Машина " + cars[i].model + " не завелась");
+            }
         }
 
 
