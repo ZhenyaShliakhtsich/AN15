8658f5c154304270ba3c93d6fbd0a21de3983b9e
ArtyomBuzuk <artyombuzuk16@gmail.com>
2022-03-07 05:56:04 +0300

    .

---
+++ src/com/teachMeSkills/an15/BuzukArtyom/hw5/Zadacha3/Main.java
@@ -0,0 +1,32 @@
+package com.teachMeSkills.an15.BuzukArtyom.hw5.Zadacha3;
+
+import java.util.Comparator;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Main {
+    public static void main(String[] args) {
+        int id = 0;
+        Set<Worker> workers = new TreeSet<>(new Comparator<Worker>() {
+            public int compare(Worker a, Worker b) {
+                double diff = a.getAveragePayment() - b.getAveragePayment();
+                if (diff > 0.0)
+                    return -1;
+                else if (diff < 0.0)
+                    return 1;
+                else {
+                    return a.getFirstName().compareTo(b.getFirstName());
+                }
+            }
+        });
+
+        workers.add(new FixZP(1, "Sergei", "Ivanov", 10000));
+        workers.add(new FixZP(2, "Ihar", "Petrov", 20000));
+        workers.add(new FixZP(3, "Dima", "Sergeev", 30000));
+        workers.add(new OnTimeZP(4, "Ivan", "Ivanov", 100));
+        workers.add(new OnTimeZP(5, "Peter", "Petrov", 150));
+
+        for (Worker w : workers)
+            System.out.println(w);
+    }
+}
---
+++ src/com/teachMeSkills/an15/BuzukArtyom/hw5/Zadacha3/Worker.java
@@ -0,0 +1,29 @@
+package com.teachMeSkills.an15.BuzukArtyom.hw5.Zadacha3;
+
+public abstract class Worker {
+    String firstName;
+    String secondName;
+    int id;
+
+    public Worker(String firstName, String secondName, int id) {
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.id = id;
+    }
+
+    String getFirstName() {
+        return firstName;
+    }
+
+    String getSecondName() {
+        return secondName;
+    }
+
+    abstract double getAveragePayment();
+
+    @Override
+    public String toString() {
+        return "№" + id + " " + firstName + " " + secondName;
+    }
+}
+
