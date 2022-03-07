e7c55904f0b964cacb7d15ca110c0dab7c9ac63d
Sergei-AN15 <tuskal92@gmail.com>
2022-03-07 20:22:32 +0300

    Задание 2 ДЗ 4

--- src/com/teachMeSkills/an15/TuskalSergey/hw4/Task2.java
+++ src/com/teachMeSkills/an15/TuskalSergey/hw4/Task2.java
@@ -1,6 +1,10 @@
-package com.teachMeSkills.an15.TuskalSergey.hw4;
+package com.company;
 
 import java.util.Scanner;
+import java.util.regex.Pattern;
+
+import static com.teachMeSkills.an15.TuskalSergey.hw4.WrongLoginException.wrongLoginMessage;
+import static com.teachMeSkills.an15.TuskalSergey.hw4.WrongPasswordException.wrongPasswordMessage;
 
 public abstract class Task2 {
     //2. Создать статический метод который принимает на вход три параметра:
@@ -17,26 +21,61 @@ public abstract class Task2 {
     //Обработка исключений проводится внутри метода.
     //Используем multi-catch block.
     //Метод возвращает true, если значения верны или false в другом случае.
-    public static String inputLoginAndPassword() {
+    public static void inputLoginAndPassword() {
+        String regEx = "\\w"; //только латинские буквы, цифры и знак подчеркивания
+        StringBuilder StringBuilder = new StringBuilder();
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите логин: ");
-        String login = scanner.next("[a-zA-Z_0-9]*");
-        System.out.println("Введите пароль: ");
-        String password = scanner.next("[a-zA-Z_0-9]*");
-        System.out.println("Подтвердите пароль: ");
-        String confirmPassword = scanner.next("[a-zA-Z_0-9]*");
-        scanner.close();
-        return login;
+        for (int i = 0; i <= 0; ) {
+            System.out.print("Введите логин: ");
+            String login = scanner.next();
+            boolean loginMatchesPattern = Pattern.matches(regEx, login);
+            try {
+                if (loginMatchesPattern == true && login.length() < 20) {
+                    i++;
+                    StringBuilder.append("Логин - ");
+                    StringBuilder.append(login);
+                } else {
+                    int a = 10 / i;
+                }
+            } catch (Exception WrongLoginException) {
+                wrongLoginMessage();
+            }
+        }
+        for (int i1 = 0; i1 <= 0; ) {
+            System.out.print("Введите пароль: ");
+            String password = scanner.next();
+            boolean passwordMatchesPattern = Pattern.matches(regEx, password);
+            try {
+                if (passwordMatchesPattern == true && password.length() < 20) {
+                    i1++;
+                    for (int i2 = 0; i2 <= 0; ) {
+                        System.out.print("Подтвердите пароль: ");
+                        String confirmPassword = scanner.next();
+                        boolean confirmedPassword = confirmPassword.equals(password);
+                        if (confirmedPassword == true) {
+                            i2++;
+                            StringBuilder.append(", пароль - ");
+                            StringBuilder.append(password);
+                            System.out.println(StringBuilder);
+                        } else System.out.println("Пароль должен совпадать с введеным ранее.");
+                    }
+                } else {
+                    int a = 10 / i1;
+                }
+            } catch (Exception WrongPasswordException) {
+                wrongPasswordMessage();
+            }
+        }
     }
+
+
     public static void main(String[] args) {
-
-
-
+        inputLoginAndPassword();
     }
 
+}
 
 
 
-    }
 
 
