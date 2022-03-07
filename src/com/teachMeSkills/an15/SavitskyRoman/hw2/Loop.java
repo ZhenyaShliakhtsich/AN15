ea866507e207b7337e426b6fdd6a33ff9b830101
SavitskyRoman <savitskyroman508@gmail.com>
2022-03-08 02:06:09 +0300

    орфография

--- src/com/teachMeSkills/an15/SavitskyRoman/hw2/Loop.java
+++ src/com/teachMeSkills/an15/SavitskyRoman/hw2/Loop.java
@@ -1,4 +1,5 @@
 package com.teachMeSkills.an15.SavitskyRoman.hw2;
+
 /*Задача на циклы
 Необходимо, чтоб программа выводила на экран вот такую последовательность:
 5 10 15 20 25 30 35 40 45 50 55 60 65 70 75 80 85 90 95 100
@@ -14,9 +15,9 @@ package com.teachMeSkills.an15.SavitskyRoman.hw2;
 Вывести в консоль таблицу умножения для числа 7. Пример 7 * 1 = 7
 7 * 2 = 14 и тд до 7 * 10 = 70*/
 public class Loop {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         StringBuilder stringBuilder = new StringBuilder();
-        for (int x = 5; x <= 100; x += 5){
+        for (int x = 5; x <= 100; x += 5) {
             stringBuilder.append(x);
             if (x == 100) continue;
             stringBuilder.append(" ");
@@ -24,16 +25,16 @@ public class Loop {
         System.out.println(stringBuilder + "\n");
 
         int luckyCent = (int) (Math.random() * 30 + 1);
-        for (int x = 1; x <= 30; x++){
+        for (int x = 1; x <= 30; x++) {
             if (x == luckyCent) {
                 System.out.println("Счастливая монета номер " + x + " найдена\n");
                 break;
             }
-                System.out.println("Надеюсь следующая будет счастливая " + x);
+            System.out.println("Надеюсь следующая будет счастливая " + x);
 
         }
 
-        for (int x = 1 ; x <= 10; x++){
+        for (int x = 1; x <= 10; x++) {
             if (x == 10) {
                 System.out.println(x + "\n");
                 continue;
@@ -41,7 +42,7 @@ public class Loop {
             System.out.print(x + " ");
         }
 
-        for (int x = 10 ; x >= 0; x--){
+        for (int x = 10; x >= 0; x--) {
             if (x == 0) {
                 System.out.println(x + "\n");
                 continue;
@@ -49,7 +50,7 @@ public class Loop {
             System.out.print(x + " ");
         }
 
-        for (int x = 1; x <= 10; x++){
+        for (int x = 1; x <= 10; x++) {
             int y = 7;
             int z = y * x;
             System.out.println(y + " * " + x + " = " + z);
