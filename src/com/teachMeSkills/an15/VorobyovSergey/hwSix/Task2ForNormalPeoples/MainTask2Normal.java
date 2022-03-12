22b8c6ae208523b093f0d78f6d59acfab57839aa
VorobyovSergei <sergei.vorobyov@mail.ru>
2022-03-13 01:13:24 +0300

    Hw6 Task2New так можно бы и попроще

---
+++ src/com/teachMeSkills/an15/VorobyovSergey/hwSix/Task2ForNormalPeoples/MainTask2Normal.java
@@ -0,0 +1,14 @@
+package com.teachMeSkills.an15.VorobyovSergey.hwSix.Task2ForNormalPeoples;
+
+import java.util.ArrayList;
+
+public class MainTask2Normal {
+    public static void main(String[] args) {
+        ArrayList<MyInstrumentService> list = new ArrayList<>();
+        list.add(new MyDrum(2));
+        list.add(new MyHorn(1));
+        list.add(new MyGuitare(6));
+
+        list.stream().forEach(e -> e.play());
+    }
+}
