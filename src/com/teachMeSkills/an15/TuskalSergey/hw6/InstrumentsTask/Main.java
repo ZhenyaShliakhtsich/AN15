6085b53f7bd4ddbd95cf9b2a540655362b7fcf3f
Sergei-AN15 <tuskal92@gmail.com>
2022-03-19 14:21:20 +0300

    ДЗ-6 задание 2 доделано.

--- src/com/teachMeSkills/an15/TuskalSergey/hw6/InstrumentsTask/Main.java
+++ src/com/teachMeSkills/an15/TuskalSergey/hw6/InstrumentsTask/Main.java
@@ -1,15 +1,24 @@
-/*
 package com.teachMeSkills.an15.TuskalSergey.hw6.InstrumentsTask;
 
 import com.teachMeSkills.an15.TuskalSergey.hw6.InstrumentsTask.instruments.Baraban;
 import com.teachMeSkills.an15.TuskalSergey.hw6.InstrumentsTask.instruments.Guitar;
 import com.teachMeSkills.an15.TuskalSergey.hw6.InstrumentsTask.instruments.Instrument;
 import com.teachMeSkills.an15.TuskalSergey.hw6.InstrumentsTask.instruments.Truba;
+import com.teachMeSkills.an15.TuskalSergey.hw6.InstrumentsTask.services.BarabanInstrumentServiceImpl;
 import com.teachMeSkills.an15.TuskalSergey.hw6.InstrumentsTask.services.InstrumentService;
+import com.teachMeSkills.an15.TuskalSergey.hw6.InstrumentsTask.services.TrubaInstrumentServiceImpl;
+import com.teachMeSkills.an15.TuskalSergey.hw6.InstrumentsTask.services.impl.GuitarInstrumentServiceImpl;
 
 public class Main {
 
     public static void main(String[] args) {
+        InstrumentService guitarService = new GuitarInstrumentServiceImpl();
+        InstrumentService barabanService = new BarabanInstrumentServiceImpl();
+        InstrumentService trubaService = new TrubaInstrumentServiceImpl();
+
+        InstrumentService[] instrumentServices = {guitarService, barabanService, trubaService};
+
+
         Instrument guitar = new Guitar();
         guitar.setCountStrun(5);
         Instrument baraban = new Baraban();
@@ -19,15 +28,9 @@ public class Main {
 
         Instrument[] instruments = {guitar, baraban, truba};
 
-        for (int i = 0; i < instruments.length; i++) {
-            InstrumentService.play(instruments[i]);
+        for (int i = 0; i < instrumentServices.length; i++) {
+            instrumentServices[i].play(instruments[i]);
         }
     }
 
 }
-
-
-
-
-
-*/
