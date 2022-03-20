eef18fd111a1fd1e009829e205888c54113ab573
VorobyovSergei <sergei.vorobyov@mail.ru>
2022-03-20 15:08:22 +0300

    Pilyushin Shlyakhtich Vorobyov делаем Hw7 Task2

--- src/com/teachMeSkills/an15/teamwork/hw7MakarkaAndDrazdroperma/MandDMain.java
+++
@@ -1,4 +0,0 @@
-package com.teachMeSkills.an15.teamwork.hw7MakarkaAndDrazdroperma;
-
-public class MandDMain {
-}
---
+++ src/com/teachMeSkills/an15/teamwork/hw7PilyushinShlyakhtichVorobyov/MainPSV.java
@@ -0,0 +1,4 @@
+package com.teachMeSkills.an15.teamwork.hw7PilyushinShlyakhtichVorobyov;
+
+public class MainPSV {
+}
