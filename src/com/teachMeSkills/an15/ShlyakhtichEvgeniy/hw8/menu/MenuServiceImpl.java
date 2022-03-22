716913e0874cfb9de63990d432f00ba471c038cf
ZhenyaShliakhtsich <shl.jeka@gmail.com>
2022-03-22 19:28:28 +0300

    Задание 1

--- src/com/teachMeSkills/an15/ShlyakhtichEvgeniy/hw8/menu/MenuServiceImpl.java
+++ src/com/teachMeSkills/an15/ShlyakhtichEvgeniy/hw8/menu/MenuServiceImpl.java
@@ -9,6 +9,7 @@ import com.teachMeSkills.an15.ShlyakhtichEvgeniy.hw8.service.impl.UserServiceImp
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class MenuServiceImpl implements MenuService {
@@ -19,85 +20,101 @@ public class MenuServiceImpl implements MenuService {
     public void adminMenu(User user, ArrayList<Product> products, HashMap<String, User> users) {
         System.out.println("1.Добавить запчасть\n2.Изменить запчасть\n3.Удалить запчать.\n4.Выйти из пользователя" +
                 "\n5.Выйти из программы");
-        int choice = scanner.nextInt();
-        switch (choice) {
-            case 1:
-                userService.addProduct(products);
-                adminMenu(user, products, users);
-                break;
-            case 2:
-                userService.changeProduct(products);
-                adminMenu(user, products, users);
-                break;
-            case 3:
-                userService.deleteProduct(products);
-                adminMenu(user, products, users);
-                break;
-            case 4:
-                authMenu(user, products, users);
-                break;
-            case 5:
-                break;
-            default:
-                adminMenu(user, products, users);
-        }
+       try {
+           int choice = scanner.nextInt();
+           switch (choice) {
+               case 1:
+                   userService.addProduct(products);
+                   adminMenu(user, products, users);
+                   break;
+               case 2:
+                   userService.changeProduct(products);
+                   adminMenu(user, products, users);
+                   break;
+               case 3:
+                   userService.deleteProduct(products);
+                   adminMenu(user, products, users);
+                   break;
+               case 4:
+                   authMenu(user, products, users);
+                   break;
+               case 5:
+                   break;
+               default:
+                   adminMenu(user, products, users);
+           }
+
+       } catch (InputMismatchException e){
+           System.out.println("Введите номер пункта меню");
+           adminMenu(user, products, users);
+       }
 
     }
 
     public void authMenu(User user, ArrayList<Product> products, HashMap<String, User> users) {
         System.out.println("1.Авторизироваться\n2.Зарегистрироваться\n3.Выйти из программы");
-        int choice = scanner.nextInt();
-        switch (choice) {
-            case 1:
-                if (authService.login(users).isHasAdminRole()) {
-                    adminMenu(user, products, users);
-                } else {
-                    userMenu(user, users, products);
-                }
-                break;
-            case 2:
-                authService.registration(users);
-                break;
-            case 3:
-                break;
-            default:
-                authMenu(user, products, users);
+        try {
+            int choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    if (authService.login(users).isHasAdminRole()) {
+                        adminMenu(user, products, users);
+                    } else {
+                        userMenu(user, users, products);
+                    }
+                    break;
+                case 2:
+                    authService.registration(users);
+                    break;
+                case 3:
+                    break;
+                default:
+                    authMenu(user, products, users);
 
 
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Введите номер пункта меню");
+            authMenu(user, products, users);
         }
     }
 
     public void userMenu(User user, HashMap<String, User> users, ArrayList<Product> products) {
         System.out.println("1.Поиск продуктов\n2.Выбор продукта\n3.Корзина\n4.Выйти из пользователя" +
                 "\n5.Выйти из программы");
-        int choice = scanner.nextInt();
-        switch (choice) {
-            case 1:
-                userService.searchForProducts(products);
-                userMenu(user, users, products);
-                break;
-            case 2:
-                Product currentProduct = new Product();
-                currentProduct = userService.chooseProduct(products);
-                if (currentProduct == null) {
-                    System.out.println("Такого продукта нет");
+        try {
+            int choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    userService.searchForProducts(products);
                     userMenu(user, users, products);
-                } else {
-                    productMenu(user, users, products, currentProduct);
+                    break;
+                case 2:
+                    Product currentProduct = new Product();
+                    currentProduct = userService.chooseProduct(products);
+                    if (currentProduct == null) {
+                        System.out.println("Такого продукта нет");
+                        userMenu(user, users, products);
+                    } else {
+                        productMenu(user, users, products, currentProduct);
+                        userMenu(user, users, products);
+                    }
+                    break;
+                case 3:
+                    basketMenu(user, users, products);
                     userMenu(user, users, products);
-                }
-                break;
-            case 3:
-                basketMenu(user, users, products);
-                userMenu(user, users, products);
-                break;
-            case 4:
-                authMenu(user, products, users);
-                break;
-            case 5:
-                break;
-            default:
-                userMenu(user, users, products);
+                    break;
+                case 4:
+                    authMenu(user, products, users);
+                    break;
+                case 5:
+                    break;
+                default:
+                    userMenu(user, users, products);
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Введите номер пункта меню");
+            userMenu(user, users, products);
         }
     }
 
@@ -108,19 +125,24 @@ public class MenuServiceImpl implements MenuService {
         }
         System.out.println(priceService.calculateTotalBasketPrice(user) + " руб.");
         System.out.println("1.Оплатить корзину\n2.Удалить продукт из корзины\n3.Назад");
-        int choice = scanner.nextInt();
-        switch (choice) {
-            case 1:
-                userService.payForBasket(user, products);
-                break;
-            case 2:
-                userService.deleteProductFromBasket(user);
-                basketMenu(user, users, products);
-                break;
-            case 3:
-                userMenu(user, users, products);
-            default:
-                basketMenu(user, users, products);
+        try {
+            int choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    userService.payForBasket(user, products);
+                    break;
+                case 2:
+                    userService.deleteProductFromBasket(user);
+                    basketMenu(user, users, products);
+                    break;
+                case 3:
+                    userMenu(user, users, products);
+                default:
+                    basketMenu(user, users, products);
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Введите номер пункта меню");
+            basketMenu(user, users, products);
         }
     }
 
@@ -128,24 +150,29 @@ public class MenuServiceImpl implements MenuService {
         RateServiceImpl rateService = new RateServiceImpl();
         System.out.println(product.toString() + "\nРейтинг : " + rateService.calculateAvgRate(product));
         System.out.println("1.Добавить товар в корзину\n2.Оценить товар\n3.Оставить отзыв о товаре\n4.Назад");
-        int choice = scanner.nextInt();
-        switch (choice) {
-            case 1:
-                userService.addProductToBasket(user, product);
-                productMenu(user, users, products, product);
-                break;
-            case 2:
-                userService.rateProduct(product);
-                productMenu(user, users, products, product);
-                break;
-            case 3:
-                userService.commentProduct(product);
-                productMenu(user, users, products, product);
-                break;
-            case 4:
-                userMenu(user, users, products);
-            default:
-                productMenu(user, users, products, product);
+        try {
+            int choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    userService.addProductToBasket(user, product);
+                    productMenu(user, users, products, product);
+                    break;
+                case 2:
+                    userService.rateProduct(product);
+                    productMenu(user, users, products, product);
+                    break;
+                case 3:
+                    userService.commentProduct(product);
+                    productMenu(user, users, products, product);
+                    break;
+                case 4:
+                    userMenu(user, users, products);
+                default:
+                    productMenu(user, users, products, product);
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Введите номер пункта меню");
+            productMenu(user, users, products, product);
         }
     }
 }
