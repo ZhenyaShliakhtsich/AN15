f4da1e23c45e65771fec07880bbcb71c031fb431
Kakiedela <cobvscof@gmail.com>
2022-03-30 19:23:43 +0300

    qwe

---
+++ src/com/teachMeSkills/an15/LashkevichGeorgy/hw8/Main.java
@@ -0,0 +1,9 @@
+package com.teachMeSkills.an15.LashkevichGeorgy.hw8;
+//Создать руками файл в который положить какой-то текст
+//прочитать файл и удалить из него все слова, у которых длина больше 3 символов.
+// Результат записать в новый файл
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
