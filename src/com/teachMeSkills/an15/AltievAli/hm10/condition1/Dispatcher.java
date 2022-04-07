27a6d99da3726629f9da6c4a47473d30de383557
AliAltiyev <alidroid696@gmail.com>
2022-04-07 11:39:48 +0300

    final homework is done completely!

--- src/com/teachMeSkills/an15/AltievAli/hm10/condition1/Dispatcher.java
+++ src/com/teachMeSkills/an15/AltievAli/hm10/condition1/Dispatcher.java
@@ -1,30 +1,41 @@
 package com.teachMeSkills.an15.AltievAli.hm10.condition1;
 
-import com.teachMeSkills.an15.AltievAli.hm10.condition1.People;
-import com.teachMeSkills.an15.AltievAli.hm10.condition1.Sex;
-
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
+/*
+Условие: дана коллекция класс People
+(с полями name — имя, age — возраст, sex — пол),
+ вида Arrays.asList( new People(«Вася», 16, Sex.MAN), new People(«Петя», 23, Sex.MAN),
+  new People(«Елена», 42, Sex.WOMEN), new People(«Иван Иванович», 69, Sex.MAN)).
+
+ */
 public class Dispatcher {
     public static void main(String[] args) {
-
         List<People> people = Arrays.asList(new People("Вася", 16, Sex.MAN),
                 new People("Петя", 23, Sex.MAN),
                 new People("Елена", 42, Sex.WOMAN),
                 new People("Иван Иванович", 69, Sex.MAN));
 
-        List filteredList = people.stream().filter(element ->
-                element.getAge() > 18 && element.getAge() < 27).collect(Collectors.toList());
-//        System.out.println(filteredList);
+
+        double asDouble = people.stream().filter(element -> element.getAge() >= 18 ||
+                element.getAge() <= 27).mapToInt(element -> element.getAge()).average().getAsDouble();
+
+        System.out.println(asDouble);
+
 
         double averageAgeOfMen = people.stream().filter(element -> element.getSexEnum() == Sex.MAN)
                 .mapToInt(element -> element.getAge()).average().getAsDouble();
-//        System.out.println(averageAgeOfMen);
+        System.out.println(averageAgeOfMen);
 
-        Long l = people.stream().filter(element -> element.getAge() > 18 && element.getAge() < 60).count();
-        System.out.println(l);
+
+        long countOfPeopleWhoCanWork = people
+                .stream()
+                .filter(element -> element.getAge() >= 18)
+                .filter(element -> element.getSexEnum().equals(Sex.WOMAN) && element.getAge() < 55 ||
+                        element.getSexEnum().equals(Sex.MAN) && element.getAge() < 60).count();
+
+        System.out.println(countOfPeopleWhoCanWork);
 
 
     }
